7a7d75b154f8656b1335b0ab2d2cf3324716526c
Amaro9 <82824938+Amaro9@users.noreply.github.com>
2022-12-14 14:57:02 +0100

    Add files via upload

--- AI.py
+++ AI.py
@@ -1,11 +1,11 @@
 import pygame
 import random
 import sys
-import time
+import math
 from pygame.locals import *
 
 FPS = pygame.time.Clock()
-FPS.tick(60)
+FPS.tick(1)
 
 # les couleurs de BASEEEEEE
 BLUE  = (0, 0, 255)
@@ -14,11 +14,8 @@ GREEN = (0, 255, 0)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
-SCREEN_WIDTH=400
-SCREEN_HIGHT=600
-
-DISPLAYSURF = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HIGHT))
-DISPLAYSURF.fill(WHITE)
+SCREEN_WIDTH=1920
+SCREEN_HIGHT=1080
 
 
 # /!\  SPRITE PATH ATTENTION SI UN TRUC NE S'AFFICHE PAS C'EST probablement a cause de ça 
@@ -29,13 +26,14 @@ AI_PATH="S_Test.png"
 class AI_ACTOR(pygame.sprite.Sprite):
 
     speed = 1
+    suceed=False
 
     def __init__(self):
         super().__init__()
 
         self.image = pygame.image.load("S_Test.png")
         self.rect = self.image.get_rect()
-        self.rect.center=(random.randint(40,SCREEN_WIDTH-40),0)
+        self.rect.center=(SCREEN_WIDTH / 2, SCREEN_HIGHT/2)
         
 
     def update(self):
@@ -47,28 +45,47 @@ class AI_ACTOR(pygame.sprite.Sprite):
         if pressed_keys[K_DOWN]:
             self.rect.move_ip(0,1)
          
-        if self.rect.left > 0:
-              if pressed_keys[K_LEFT]:
+        if pressed_keys[K_LEFT]:
                   self.rect.move_ip(-1, 0)
-        if self.rect.right < SCREEN_WIDTH:        
-              if pressed_keys[K_RIGHT]:
+        if pressed_keys[K_RIGHT]:
                   self.rect.move_ip(1, 0)
 
 
-    def move(self):
-        self.rect.move_ip(0,10)
-        if (self.rect.bottom > 600):
-            self.rect.top = 0
-            self.rect.center = (random.randint(30, 370), 0)
+    def move(self, goal:tuple, suceed:bool):
+        
 
+        if suceed == False:
+
+            if abs(self.rect.x - goal[0]) < 15 and abs(self.rect.y - goal[1]) < 15:
+                suceed = True 
+                print("YEAHH")
+
+            if self.rect.x < goal[0]:
+                self.rect.move_ip(1, 0)
+            else:
+                self.rect.move_ip(-1, 0)
+
+            if self.rect.y < goal[1]:
+                self.rect.move_ip(0, 1)
+            else:
+                self.rect.move_ip(0, -1)
+
+            #print ("goal : " + str(self.rect.x) + " " + str(self.rect.y) + " | Pos : " + str(goal[0]) + " " + str(goal[1]) 
+            #+ str(abs(self.rect.x - goal[0])) + " " + str((self.rect.y - goal[1])))
+
+            return suceed
+
+        
+
+    
+    def FindRandomPointAtDistance(self, distance:float):
+        randomX = random.randint(self.rect.x - distance, self.rect.x + distance)
+        randomY = random.randint(self.rect.y - distance, self.rect.y + distance)
+
+        return (randomX, randomY)
+        
+        
 
     def draw(self, surface):
         surface.blit(self.image, self.rect) 
 
-
-def FindRandomPointAtDistance(distance:float):
-    randomY = random.randrange(0, distance)
-    randomX = random.randrange(0, distance)
-
-    return (randomX, randomY)
-    
--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import sys
 from pygame.locals import *
 
 FPS = pygame.time.Clock()
-FPS.tick(60)
+FPS.tick(30)
+
 
 # les couleurs de BASEEEEEE
 BLUE  = (0, 0, 255)
@@ -14,17 +15,25 @@ GREEN = (0, 255, 0)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
-SCREEN_WIDTH=1000
-SCREEN_HIGHT=1000
+SCREEN_WIDTH=1920
+SCREEN_HIGHT=1080
 
 DISPLAYSURF = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HIGHT))
 DISPLAYSURF.fill(WHITE)
 
+AI.SCREEN_HIGHT = SCREEN_HIGHT
+AI.SCREEN_WIDTH = SCREEN_WIDTH
 
 
 A1 = AI.AI_ACTOR()
 A2 = AI.AI_ACTOR()
+A3 = AI.AI_ACTOR()
+A4 = AI.AI_ACTOR()
 
+
+goal1 = A2.FindRandomPointAtDistance(50)
+goal2 = A3.FindRandomPointAtDistance(50)
+goal3 = A4.FindRandomPointAtDistance(50)
 
 
 while True:     
@@ -33,12 +42,36 @@ while True:
             pygame.quit()
             sys.exit()
     A1.update()
-    A2.move()
+
+    if A2.suceed == False:
+        A2.suceed = A2.move(goal1, A2.suceed)
+    else:
+        goal1 = A2.FindRandomPointAtDistance(200)
+        A2.suceed = False
+
+        
+    if A3.suceed == False:
+        A3.suceed = A3.move(goal2, A3.suceed)
+    else:
+        goal2 = A3.FindRandomPointAtDistance(200)
+        A3.suceed = False
+
+        
+    if A4.suceed == False:
+        A4.suceed = A4.move(goal3, A4.suceed)
+    else:
+        goal3 = A4.FindRandomPointAtDistance(200)
+        A4.suceed = False
+
+        
 
      
     DISPLAYSURF.fill(WHITE)
     
     A1.draw(DISPLAYSURF)
     A2.draw(DISPLAYSURF)
+    A3.draw(DISPLAYSURF)
+    A4.draw(DISPLAYSURF)
     pygame.display.update()
     
+
