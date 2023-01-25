81b67daa85736001b39423714ffd98fdf809f092
Amaro9 <82824938+Amaro9@users.noreply.github.com>
2023-01-25 16:21:04 +0100

    vroom

--- AI.py
+++ AI.py
@@ -5,32 +5,12 @@
 # ༼ つ ◕_◕ ༽つ
 
 
-import pygame # https://www.pygame.org
 import random
-import sys
 import math
-from pygame.locals import *
-
-FPS = pygame.time.Clock() # https://www.geeksforgeeks.org/pygame-time/ (pour tout ce qui touche au FPS)
-FPS.tick(1)
-
-# les couleurs de BASEEEEEE
-BLUE  = (0, 0, 255)
-RED   = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-
-SCREEN_WIDTH=1920
-SCREEN_HIGHT=1080
-
-
-# /!\  SPRITE PATH ATTENTION SI UN TRUC NE S'AFFICHE PAS C'EST probablement a cause de ça 
-AI_PATH="S_Test.png"
 
 
 
-class AI_ACTOR(pygame.sprite.Sprite): #https://learnpython.com/blog/custom-class-python/
+class AI_ACTOR(): 
 
     speed = 1
     suceed=False
@@ -38,62 +18,8 @@ class AI_ACTOR(pygame.sprite.Sprite): #https://learnpython.com/blog/custom-class
     def __init__(self):
         super().__init__()
 
-        self.image = pygame.image.load("S_Test.png")
-        self.rect = self.image.get_rect()
-        self.rect.center=(SCREEN_WIDTH / 2, SCREEN_HIGHT/2)
-        
-
-    def update(self):
-        
-        #https://stackoverflow.com/questions/35136091/pygame-key-getting-pressed (pour les touches)
-        #https://stackoverflow.com/questions/16183265/how-to-move-sprite-in-pygame (pour bouger un sprite)
-        pressed_keys = pygame.key.get_pressed()
-        if pressed_keys[K_UP]:
-            self.rect.move_ip(0, -1)
-        if pressed_keys[K_DOWN]:
-            self.rect.move_ip(0,1)
-         
-        if pressed_keys[K_LEFT]:
-                  self.rect.move_ip(-1, 0)
-        if pressed_keys[K_RIGHT]:
-                  self.rect.move_ip(1, 0)
-
-
-    def move(self, goal:tuple, suceed:bool):
-        
-
-        if suceed == False:
-
-            if abs(self.rect.x - goal[0]) < 15 and abs(self.rect.y - goal[1]) < 15:
-                suceed = True 
-                print("YEAHH")
-
-            if self.rect.x < goal[0]:
-                self.rect.move_ip(1, 0)
-            else:
-                self.rect.move_ip(-1, 0)
-
-            if self.rect.y < goal[1]:
-                self.rect.move_ip(0, 1)
-            else:
-                self.rect.move_ip(0, -1)
-
-            #print ("goal : " + str(self.rect.x) + " " + str(self.rect.y) + " | Pos : " + str(goal[0]) + " " + str(goal[1]) 
-            #+ str(abs(self.rect.x - goal[0])) + " " + str((self.rect.y - goal[1])))
-
-            return suceed
-
-        
-
-    
     def FindRandomPointAtDistance(self, distance:float):
         randomX = random.randint(self.rect.x - distance, self.rect.x + distance)
         randomY = random.randint(self.rect.y - distance, self.rect.y + distance)
 
         return (randomX, randomY)
-        
-        
-    #https://www.pygame.org/docs/ref/draw.html
-    def draw(self, surface):
-        surface.blit(self.image, self.rect) 
-
--- main.py
+++ main.py
@@ -5,81 +5,29 @@
 # ༼ つ ◕_◕ ༽つ
 
 
+
 import AI #Un sripte custome (ont peut faire ça si vous saviez po ╰(*°▽°*)╯
-import pygame
+import turtle
+import functools
 import random
 import sys
-from pygame.locals import *
 
-FPS = pygame.time.Clock()
-FPS.tick(30)
+screen = turtle.Screen()
+superTurtle = turtle.Turtle()
 
+superTurtle.shape("circle")
 
-# les couleurs de BASEEEEEE
-BLUE  = (0, 0, 255)
-RED   = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-
-SCREEN_WIDTH=1920
-SCREEN_HIGHT=1080
-
-DISPLAYSURF = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HIGHT))
-DISPLAYSURF.fill(WHITE)
-
-AI.SCREEN_HIGHT = SCREEN_HIGHT
-AI.SCREEN_WIDTH = SCREEN_WIDTH
+def test():
+    print("VROOM")
 
 
-A1 = AI.AI_ACTOR() #Faut aller voir l'autre script pour bien comprendre
-A2 = AI.AI_ACTOR()
-A3 = AI.AI_ACTOR()
-A4 = AI.AI_ACTOR()
+screen.listen()
+screen.onkey()
 
 
-goal1 = A2.FindRandomPointAtDistance(50)
-goal2 = A3.FindRandomPointAtDistance(50)
-goal3 = A4.FindRandomPointAtDistance(50)
+while True:
+
+    screen.update()
 
 
-while True:   #Une update de base 
-    
-    for event in pygame.event.get():              
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-    A1.update()
-
-    if A2.suceed == False:
-        A2.suceed = A2.move(goal1, A2.suceed)
-    else:
-        goal1 = A2.FindRandomPointAtDistance(200)
-        A2.suceed = False
-
-        
-    if A3.suceed == False:
-        A3.suceed = A3.move(goal2, A3.suceed)
-    else:
-        goal2 = A3.FindRandomPointAtDistance(200)
-        A3.suceed = False
-
-        
-    if A4.suceed == False:
-        A4.suceed = A4.move(goal3, A4.suceed)
-    else:
-        goal3 = A4.FindRandomPointAtDistance(200)
-        A4.suceed = False
-
-        
-
-     
-    DISPLAYSURF.fill(WHITE)
-    
-    A1.draw(DISPLAYSURF)
-    A2.draw(DISPLAYSURF)
-    A3.draw(DISPLAYSURF)
-    A4.draw(DISPLAYSURF)
-    pygame.display.update()
-    
 
