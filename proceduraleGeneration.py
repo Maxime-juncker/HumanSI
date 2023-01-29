a8a5fb2e70157fc0ef7e7f33a59064815901a677
Chahalor <119660824+Chahalor@users.noreply.github.com>
2023-01-29 19:20:27 +0100

    ok ok

--- proceduraleGeneration.py
+++ proceduraleGeneration.py
@@ -19,14 +19,16 @@ class ChunkDeBase() :
         chunkPos["Chunk " + currentChunk].append(pos())
     setpos(int(20),int(80))
     print(chunkPos)
-    ChunkCoinFinal["Chunk"] = chunkPos
+    #ChunkCoinFinal["Chunk"] = chunkPos test un peux rater
     end_fill()
     done()
 
 print(chunkPos)
 
 def GenerationMap() :
-    
-
-
- return None
+    #color('red')
+    begin_fill()
+    setpos(int(chunkPos['Chunk 0'](1)))
+    end_fill
+    print("ok ok")
+GenerationMap
