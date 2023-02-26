fd3e59ea8878cdefbf89f952f82a8d8dd0f531bc
Chahalor <119660824+Chahalor@users.noreply.github.com>
2023-02-26 11:53:07 +0100

    salut a tous  c est fanta

--- proceduraleGeneration.py
+++ proceduraleGeneration.py
@@ -7,9 +7,22 @@ def turtelColor(value) :
     pixelType = None
     if value == 0 :
         pixelType = 'DeepWater'
-    elif value>0 and value<1 :
+    elif value>0 and value<=0.1 :
         pixelType = 'Water'
-    elif value>1 and value<2 :
+    elif value>0.1 and value<=0.2 :
         pixelType = 'Bitche'
-    elif value>2 and value<3 :
-        pixelType = 'Medaw'
+    elif value>0.2 and value<=0.5 :
+        pixelType = 'Meadow'
+    elif value>0.5 and value<=0.7 :
+        pixelType = 'Forest'
+    elif value>0.7 and value<=0.8 :
+        pixelType = 'Hill'
+    elif value>0.8 and value<=0.9 :
+        pixelType = 'Mountain'
+    elif value>0.9 and value<=1 :
+        pixelType = 'HightMontain' 
+    return(pixelType)
+
+
+print(perlinNoiseValue)
+print(turtelColor(perlinNoiseValue))
--- test.py
+++ test.py
@@ -1,33 +1,5 @@
-"""
-test = { 
-
-}
-test['sus'] = (10,20), (30,40), (50,60)
-print(test['sus'][1])
-
-sus = 0
-print(''+ str(sus))
-NbrChunk = 0
-nombre = str(NbrChunk)
-print(nombre)
-"""
-
-"""
 from turtle import *
 
-forward(50)
-done()
-""" 
 
-import time
-#import proceduraleGeneration
-
-fps = 50
-time_delta = 1./fps
-
-
-while True:
-    t0 = time.perf_counter()
-    time.sleep(time_delta)
-    t1 = time.perf_counter()
-    print( str(1. / (t1 - t0)))
+screensize(1080, 1080)
+forward(100)
