6535f2cfae4a45afa0d60c20d5b2af67c938ac7f
Chahalor <119660824+Chahalor@users.noreply.github.com>
2023-03-10 09:21:03 +0100

    sus

--- proceduraleGeneration.py
+++ proceduraleGeneration.py
@@ -4,32 +4,6 @@ from random import *
 from tkinter import *
 import random as rand
 
-"""
-perlinNoiseValue = random()
-
-def turtelColor(value) :
-    pixelType = None
-    if value == 0 :
-        pixelType = 'DeepWater'
-    elif value>0 and value<=0.1 :
-        pixelType = 'Water'
-    elif value>0.1 and value<=0.2 :
-        pixelType = 'Bitche'
-    elif value>0.2 and value<=0.5 :
-        pixelType = 'Meadow'
-    elif value>0.5 and value<=0.7 :
-        pixelType = 'Forest'
-    elif value>0.7 and value<=0.8 :
-        pixelType = 'Hill'
-    elif value>0.8 and value<=0.9 :
-        pixelType = 'Mountain'
-    elif value>0.9 and value<=1 :
-        pixelType = 'HightMontain' 
-    return(pixelType)
-
-print(perlinNoiseValue)                            
-print(turtelColor(perlinNoiseValue))
-"""
 
 root = Tk()
 dimX=1000
