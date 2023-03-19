c208a06585d5ece77f1bd464420e70f27f419590
Amaro9 <82824938+Amaro9@users.noreply.github.com>
2023-03-19 19:07:29 +0100

    ok

--- proceduraleGeneration.py
+++ proceduraleGeneration.py
@@ -1,144 +1,147 @@
-from turtle import *
-#from main import * 
-from random import *
 from tkinter import *
 import random as rand
 
-
 root = Tk()
-dimX=1000
-dimY=500
-canvas = Canvas(root, bg="blue", height= dimY, width= dimX)
-canvas.pack()
+resX = 1280; resY = 720
+canvas = Canvas(root, bg="blue", height=resY, width=resX); canvas.pack()
 
-Carte = {'xy': [200, 100],'cLevel': 0,'nodes': [],'seed': 10,'Choice': [-100, 200]}
+map = {
+    'xy': [300, 250],
+    'cLevel': 0,
+    'nodes': [],
+    'seed': 10,
+    'Choice': [-100, 200]
+}
 
-class node : 
-    def commencement (self, xy):
-        self.el = 0
+class node:
+    def __init__(self, xy):
         self.xy = xy
-        self.temp = 0
+        self.el = 0
+        self.temp = 0         
         self.neighbors = []
-        self.active = True
-    def couleur_pixel(self):
+        self.active = True   
+    def render(self):
         if self.el <= (-25):
-            couleur = 'bleu fonce'                              #océan
-        elif self.el <= Carte['cLevel']:
-            couleur = 'bleue'                                   #mer
-        elif self.el <= 25 and self.temp <= 0 :
-            couleur = 'gris fonce'                              #Haute montagne
-        elif self.el <= 25 and self.temp <= 5 :
-            couleur = 'gris '                                   #montagne
-        elif self.el <= 25 and self.temp <= 10 :
-            couleur = 'vert'                                    #prairie
-        elif self.el <= 25 and self.temp <= 50 : 
-            couleur = 'vert fonce'                              #forêt
-        elif self.el <= 25 and self.temp <= 100:
-            couleur = 'jaune'                                   #plage
-        else : 
-            couleur = 'marron'                                  #marais
-        a = self.xy[0] * 5
-        b = self.xy[1] * 5
-        c = a + 5 
-        d = b + 5
-        canvas.create_rectangle(a,b,c,d, fill = couleur, outline = couleur)
+            color = 'dark blue'
+        elif self.el <= map['cLevel']:
+            color = 'blue'
+        elif self.el <= 25 and self.temp <=0:
+            color = 'light grey'
+        elif self.el <= 25 and self.temp <=50:
+            color = 'green'
+        elif self.el <= 25 and self.temp <=75:
+            color = 'light green'
+        elif self.el <= 25 and self.temp <=100:
+            color = 'grey'
+        else:
+            color = 'dark grey'
+        a = self.xy[0]*5; b = self.xy[1]*5
+        c = a + 5; d = b + 5
+        canvas.create_rectangle(a,b,c,d, fill= color, outline= color)
 
-def generationNode() :
-    for i in range(Carte['xy'][0]):
-        for j in range(Carte['xy'][1]):
-            Carte['nodes'].append(node([i,j]))
-print('Génération du monde')
-generationNode()
+def genNodes():
+    for i in range(map['xy'][0]):
+        for j in range(map['xy'][1]):
+            map['nodes'].append(node([i,j]))
+print('Gen')
+genNodes()
 
-def PlusProcheVoisins():
+def getNeighbors():
     temp = []
-    for i in Carte['nodes']:
-        temp.append(i)
-    for j in Carte['nodes']:
+    for n in map['nodes']:
+        temp.append(n)
+    for n1 in map['nodes']:
         temp.pop(0)
-        for k in temp :
-            if j != k :
-                if j.xy[0] == k.xy[0]:
-                    if k.xy[1] == j.xy[1] + 1 :
-                        j.neighbors.append(k)
-                elif j.xy[1] == k.xy[1] : 
-                    if k.xy[0] == j.xy[0] + 1 :
-                        j.neighbors.append(k)
-                if len(j.neighbors) >= 2:
+        for n2 in temp:
+            if n1 != n2:
+                if n1.xy[0] == n2.xy[0]:
+                    if n2.xy[1] == n1.xy[1] + 1:
+                        n1.neighbors.append(n2)
+                elif n1.xy[1] == n2.xy[1]:
+                    if n2.xy[0] == n1.xy[0] + 1:
+                        n1.neighbors.append(n2)
+                if len(n1.neighbors) >= 2:
                     break
-print('Generations des PlusProcheVoisins')
-PlusProcheVoisins()
 
-def SetActive():
-    for i in Carte['nodes'] :
-        i.active = True
+print('Getting Neighbors')
+getNeighbors()
 
-def CartePlusBelle():
-        for j in Carte['nodes']:
-            j.active = False
-        for k in j.neighbors:
-            if k.active == True:
-                if j.el != k.el:
+def seed():
+    for n in map['nodes']:
+        if 40 < n.xy[0] < 160:
+            if 20 < n.xy[1] < 80:
+                if rand.randint(0, 1000) <= map['seed']:
+                    n.el = rand.choice(map['Choice'])
+                    n.temp = rand.choice(map['Choice'])
+
+print('Seeding')
+seed()
+
+def setActive():
+    for n in map['nodes']:
+        n.active = True
+
+def smoothMap():
+    for n1 in map['nodes']:
+        n1.active = False
+        for n2 in n1.neighbors:
+            if n2.active == True:
+                if n1.el != n2.el:
                     if rand.randrange(0, 100) < 5:
-                        a = (j.el + k.el)/2
-                        j.el = a
-                        k.el = a
+                        a = (n1.el + n2.el)/2
+                        n1.el = a
+                        n2.el = a
                     elif rand.randrange(0, 100) < 5:
-                        a = (j.temp + k.temp)/2
-                        j.temp = a
-                        k.temp = a
+                        a = (n1.temp + n2.temp)/2
+                        n1.temp = a
+                        n2.temp = a
 
-def TerrainPlusHaut():
-    for i in Carte['nodes']:
-        if i.el > 0:
+def raiseLand():
+    for n in map['nodes']:
+        if n.el > 0:
             if rand.randrange(0, 100) < 1:
-                i.el += 100
-
-def MerPlusBasse():
-    for i in Carte['nodes']:
-        if i.el <= 0:
+                n.el += 100
+def lowerSea():
+    for n in map['nodes']:
+        if n.el <= 0:
             if rand.randrange(0, 100) < 1:
-                i.el -= 100
-
-def RaiseTemp():
-    for i in Carte['nodes']:
-        if i.el > 0:
+                n.el -= 100
+def raiseTemp():
+    for n in map['nodes']:
+        if n.el > 0:
             if rand.randrange(0, 100) < 1:
-                i.temp += 10
-
-def process():
-    SetActive()
-    CartePlusBelle()
+                n.temp += 10
 
 def process1():
-    SetActive()
-    MerPlusBasse()
-    canvas.delete('all')
-
+    setActive()
+    smoothMap()
 def process2():
-    SetActive()
-    TerrainPlusHaut()
-  
+    setActive()
+    lowerSea()
+    canvas.delete('all')
 def process3():
-    SetActive()
-    RaiseTemp()
+    setActive()
+    raiseLand()  
+def process4():
+    setActive()
+    raiseTemp()
 
+    
 print('0')
-for i in range(100):
-    process()
-    process3()
-print('1')
-for i in range(15):
+for i in range(150):
     process1()
+    process4()
+print('1')
+for i in range(21):
     process2()
+    process3()
 print('2')
-for i in range(100):
-    process()
+for i in range(95):
+    process1()
 
 canvas.delete('all')
-for i in Carte['nodes']:
-    i.render()
+for n in map['nodes']:
+    n.render()
 canvas.update()
 
 mainloop()
-
