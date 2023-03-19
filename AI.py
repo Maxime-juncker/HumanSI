e8856c62fe1747ba18852abba2e3a1e0634652fd
Amaro9 <82824938+Amaro9@users.noreply.github.com>
2023-03-19 14:25:42 +0100

    Yohann je vais t'enculer

--- AI.py
+++ AI.py
@@ -1,227 +1,98 @@
-# Je vais partir du principe que ceux qui lise ce code ne save rien des modules 
-# que j'utilise, vue que je suis un chic type je vais mettre les liens vers 
-# la documentation que j'utilise (ou au moins un truc en rapport genre stackOverflow)
-
-# ༼ つ ◕_◕ ༽つ
-
-
 import random
-import math
-import turtle
-import Parameters
-import os
-import World
-import threading
-
-CURR_DIR = os.path.dirname(os.path.realpath(__file__))
-
-if CURR_DIR == "":
-    print(
-        World.bcolors.FAIL + "FAIL : CURR_DIR est empty ce probleme ne doit pas etre mis de coter si le jeu crach c'est a cause DE CURR_DIR" + World.bcolors.ENDC)
-else:
-    print(World.bcolors.OKGREEN + "CURR_DIR a bine été assigné : " + CURR_DIR + World.bcolors.ENDC)
+import pygame
+import Game
+from enum import Enum
+from Utilities import SeekNewPos
 
 
-class Object():  # Toutes les classes doivent dériver de celle ci (ça permet de tout centraliser)
-
-    def __init__(self):
-        pass
-
-
-# region Les Actors
-
-class AActor(Object):
-    actorTurtle: turtle.Turtle()
-    actorName: str
-
-    actorComponents = {}
-
-    def DoesComponentExist(self, componentName: str):
-        return componentName in self.actorComponents
-
-    def __init__(self):
-        self.actorTurtle = turtle.Turtle()
-
-        super().__init__()
-
-    def Setup(self, spriteName: tuple):
-        i = random.randint(0, len(spriteName) - 1)
-        self.actorTurtle.shape(spriteName[i])
-        self.actorTurtle.penup()
+class UnitState(Enum):
+    NONE = -1
+    IDLE = 0
+    MOVING = 1
 
 
-class AEntity(AActor):
-    speed = 999
-    isMoving = False
-    currentDestination: tuple
-    movingToken = 0
-
-    debugText = turtle.Turtle()
-
-    # ===========================================================================================
-    middleOfAnAction = False
-
-    Entityname = ""
-
-    def update(self):
-        for i in self.actorComponents:
-            self.actorComponents[i].Update()
-
-    def __init__(self):
-        super().__init__()
-
-        self.movingToken = random.randint(0, 10)
-        self.actorTurtle.speed(self.speed)
-
-    def CanMove(self):
-        return self.movingToken >= Parameters.MOVING_TOKEN_THRESHOLD and not self.middleOfAnAction
-
-    def MoveTo(self, newPosition: tuple):
-
-        # self.debugText.setpos(self.actorTurtle.pos())
-
-        if (not self.CanMove()):
-            return
-
-        if (not self.isMoving):
-            self.currentDestination = newPosition
-
-        if (self.actorTurtle.pos()[0] < self.currentDestination[0]):
-            self.isMoving = True
-            self.actorTurtle.goto(self.actorTurtle.pos()[0] + 2, self.actorTurtle.pos()[1])
-
-        if (self.actorTurtle.pos()[0] > self.currentDestination[0]):
-            self.isMoving = True
-            self.actorTurtle.goto(self.actorTurtle.pos()[0] - 2, self.actorTurtle.pos()[1])
-
-        if (self.actorTurtle.pos()[1] < self.currentDestination[1]):
-            self.isMoving = True
-            self.actorTurtle.goto(self.actorTurtle.pos()[0], self.actorTurtle.pos()[1] + 2)
-
-        if (self.actorTurtle.pos()[1] > self.currentDestination[1]):
-            self.isMoving = True
-            self.actorTurtle.goto(self.actorTurtle.pos()[0], self.actorTurtle.pos()[1] - 2)
-
-        if abs(self.actorTurtle.pos()[0] - self.currentDestination[0]) < 10 and abs(
-                self.actorTurtle.pos()[1] - self.currentDestination[1]) < 10:
-            self.isMoving = False
-            self.movingToken = 0
-
-            World.currentWorld.actorCurrentlyMoving -= 1
-
-    def FindRandomPointAtDistance(self, distance: float):
-
-        pos = self.actorTurtle.pos()
-        randomX = random.randint(pos[0] - distance, pos[0] + distance)
-        randomY = random.randint(pos[1] - distance, pos[1] + distance)
-
-        return (randomX, randomY)
+class BasicObject():
+    name = ""
 
 
-class AResourceNode(AActor):
-    def __init__(self):
-        super().__init__()
-
-    def Setup(self, spriteName: str = "square"):
-        print(spriteName)
-        self.actorTurtle.shape(spriteName)
-        self.actorTurtle.penup()
-
-        self.actorTurtle.shapesize(5)
-        self.actorTurtle.color("green")
-
-
-# endregion
-# region Les Component
-
-
-class CComponent(Object):
-    def __init__(self):
-        super().__init__()
-
-    def Update(self):  # doit etre implementer dans tous les class enfants
-        pass
-
-    def Setup(self):
-        pass
-
-
-class CHumainRace(CComponent):
-    InteractionType = {
-        1: "Talk"
-    }
+class Unit(pygame.sprite.Sprite, BasicObject):
 
     def Update(self):
-        pass
+        self.display.blit(self.image, self.rect)
 
-    actorTurtle: turtle.Turtle()
+        self.MoveTo(self.currentDestination)
 
-    def __init__(self):
+    def __init__(self, _display):
         super().__init__()
+        self.image = pygame.image.load("Assets/Pop1c.png")
+        self.rect = self.image.get_rect()
+        self.speed = 1
+        self.moveTimer = 100
+        self.display = _display
+        self.name = str(random.randint(0, 1000))
 
-    def Setup(self, actorTurlte):
-        self.actorTurtle = actorTurlte
+        Game.game.visibleSprite[self.name] = self
+
+        self.rect.x = pygame.mouse.get_pos()[0]
+        self.rect.y = pygame.mouse.get_pos()[1]
 
 
-class CAnimator(CComponent):
-    turtleToAnimate: turtle.Turtle()
-    IsOn = False
-    tileSheet: tuple
-    currentTileIndex = 0
+        self.currentDestination = SeekNewPos(self.rect, 500)
+        self.state = UnitState.MOVING
 
-    timer: threading.Timer
+    def SetNewState(self, newState: UnitState):
+        '''
+        Fonct pour changer le state
+        renvois True si le state est changer
+                False si le state reste le même
+        '''
 
-    def Setup(self, turtle, _tileSheet):
-        super().Setup()
-        self.turtleToAnimate = turtle
-        self.tileSheet = _tileSheet
+        if (not newState == self.state):
+            self.state = newState
+            return False
+        else:
+            self.state = newState
+            return True
 
-    def Update(self):
+    def MoveRight(self, dir):
+        '''
+        fonct pour move un sprite
+        dir = la direction (ex : dir = 1 donc vers la right)
+                           (     dir = -1 donc vers la gauche)
+        '''
 
-        if self.IsOn:
+        self.rect.x += self.speed * dir
+
+    def MoveUp(self, dir):
+        '''
+        fonct pour move un sprite
+        dir = la direction (ex : dir = -1 donc vers la right)
+                           (     dir = 1 donc vers la gauche)
+        '''
+
+        self.rect.y += self.speed * dir
+
+    def MoveTo(self, coord):
+
+        if (not self.state == UnitState.MOVING):
+            return
+
+        if abs(self.rect.y - coord[1]) < 5 and abs(self.rect.x - coord[0]) < 5:
+            self.SetNewState(UnitState.IDLE)
+            return
+
+        self.moveTimer -= 1
+
+        if self.moveTimer > 0:
             return
         else:
-            self.IsOn = True
-        timer = threading.Timer(0.5, self.AdvanceAnim)
-        timer.start()
+            self.moveTimer = 10
 
-
-    def AdvanceAnim(self):
-
-        self.IsOn = False
-        print(self.turtleToAnimate)
-
-        if self.currentTileIndex == len(self.tileSheet)-1:
-            self.currentTileIndex = 0
-        else:
-            self.currentTileIndex += 1
-        #self.turtleToAnimate.shape(self.tileSheet[self.currentTileIndex])
-        """
-        if self.currentTileIndex == len(self.tileSheet)-1:
-            self.currentTileIndex = 0
-        else:
-            print(self.currentTileIndex)
-            self.currentTileIndex += 1
-        self.IsOn = False"""
-
-
-class CRessource(CComponent):
-
-    def __init__(self):
-        super().__init__()
-
-
-class CDamageable(CComponent):
-    maxHp = 100
-    currentHp: maxHp
-
-    def OnDamaged(self, amount: int):
-        self.currentHp -= amount
-
-        if (self.currentHp <= 0):
-            self.OnDie()
-
-    def OnDie(self):
-        self.currentHp = 0
-
-# endregion
+        if self.rect.x <= coord[0]:
+            self.MoveRight(1)
+        elif self.rect.x >= coord[0]:
+            self.MoveRight(-1)
+        if self.rect.y <= coord[1]:
+            self.MoveUp(1)
+        elif self.rect.y >= coord[1]:
+            self.MoveUp(-1)
---
+++ Game.py
@@ -0,0 +1,31 @@
+import pygame
+import AI
+from AI import *
+
+spriteResources = {
+    "basicHuman": "Assets/Pop1c.png",
+    "Stone": "Asset/caillou1.png",
+}
+
+
+class Game:
+    visibleSprite = {}
+
+    def __init__(self):
+        # Creation de la fenetre de l'app
+
+        self.newUnit = None
+        pygame.init()
+
+        pygame.display.set_caption("HumainSI")
+        self.display = pygame.display.set_mode((1220, 1080))
+
+        self.background = pygame.image.load("Assets/download.jpg")
+
+        self.GAME_RUNNING = True
+
+    def SpawnUnit(self):
+        newUnit = Unit(self.display)
+
+
+game = Game()
