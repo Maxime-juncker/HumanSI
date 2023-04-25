5ced263492e15c043554d8bad01bae57426f924a
Chahalor <119660824+Chahalor@users.noreply.github.com>
2023-04-25 19:24:51 +0200

    sa fait plus se qui  faut  ?

--- geneObject.py
+++ geneObject.py
@@ -1,4 +1,4 @@
-def listCoord(limX, limY, path) :   #Je fait une boucle qui à une valeur x vas associer tout les valeurs y
+"""def listCoord(limX, limY, path) :   #Je fait une boucle qui à une valeur x vas associer tout les valeurs y
     ListCoord = []
     for x in range(limX) :
         miniListCoord = [x]
@@ -7,4 +7,14 @@ def listCoord(limX, limY, path) :   #Je fait une boucle qui à une valeur x vas 
             ListCoord.append(miniListCoord)
             miniListCoord = [x]
     return ListCoord
-print(listCoord(10, 10, 1))
+#print(listCoord(10, 10, 1))"""
+
+
+from random import*
+def coordRandInList(coordX, coordY, nbrCoord) :
+    listCoord = []
+    for i in range(nbrCoord) :
+        listCoord.append((randint(0, coordX+1), randint(0, coordY+1)))
+    return(listCoord)
+
+print(coordRandInList(3000,2000, 100))
