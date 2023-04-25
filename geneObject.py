ed2eccf6bce99a0649cf6a5fdf7e64fecddc3e7e
Chahalor <119660824+Chahalor@users.noreply.github.com>
2023-04-25 19:41:25 +0200

    juste un peu d'explication
    et un peut de vérification pour qu'il n'y est pas 2 fois les même coords

--- geneObject.py
+++ geneObject.py
@@ -1,20 +1,14 @@
-"""def listCoord(limX, limY, path) :   #Je fait une boucle qui à une valeur x vas associer tout les valeurs y
-    ListCoord = []
-    for x in range(limX) :
-        miniListCoord = [x]
-        for y in range(limY) :
-            miniListCoord.append(y)
-            ListCoord.append(miniListCoord)
-            miniListCoord = [x]
-    return ListCoord
-#print(listCoord(10, 10, 1))"""
-
-
 from random import*
-def coordRandInList(coordX, coordY, nbrCoord) :
+def coordRandInList(coordX, coordY, nbrCoord) :     #comme parametre on met les coordoner des extremiter de la map et le nombre de coordonnée renvoyer
     listCoord = []
     for i in range(nbrCoord) :
-        listCoord.append((randint(0, coordX+1), randint(0, coordY+1)))
+        x = randint(0, coordX+1)    #met en argument des nombres aléatoires
+        y = randint(0, coordY+1)
+        try :
+            listCoord.index((x, y))     #verifie qu'il n'y a pas  2 fois les meme coords
+            pass
+        except :
+            listCoord.append((x, y))  
     return(listCoord)
 
-print(coordRandInList(3000,2000, 100))
+print(coordRandInList(3000,2000, 200))
